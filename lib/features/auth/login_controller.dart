e60ba3f81ec4547ba5df4070ff7b585ceb118f30
raihanaiooo <raihanaaaz2@gmail.com>
2026-02-16 09:58:22 +0700

    Task 1 Done

--- lib/features/auth/login_controller.dart
+++ lib/features/auth/login_controller.dart
@@ -1,13 +1,12 @@
 class LoginController {
   // Database hardcoded untuk validasi login
-  final String _validUsername = "admin";
-  final String _validPassword = "123";
+
+  Map<String, String> _users = {"admin": "123", "sasa": "sasa123"};
+  // final String _validUsername = "admin";
+  // final String _validPassword = "123";
 
   // Fungi pengecekan (logic only)
   bool login(String username, String password) {
-    if (username == _validUsername && password == _validPassword) {
-      return true;
-    }
-    return false;
+    return _users[username] == password;
   }
 }
--- lib/features/auth/login_view.dart
+++ lib/features/auth/login_view.dart
@@ -9,6 +9,8 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  int _attempts = 0;
+  bool _isLocked = false;
   final LoginController _controller = LoginController();
   final TextEditingController _userController = TextEditingController();
   final TextEditingController _passController = TextEditingController();
@@ -17,20 +19,55 @@ class _LoginViewState extends State<LoginView> {
     String user = _userController.text;
     String pass = _passController.text;
 
+    if (user.isEmpty || pass.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Username dan Password tidak boleh kosong!"),
+        ),
+      );
+      return;
+    }
+
     bool isSuccess = _controller.login(user, pass);
 
     if (isSuccess) {
+      _attempts = 0;
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => CounterView(username: user)),
       );
     } else {
+      _attempts++;
+
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
           content: Text("Login Gagal! Username atau Password salah"),
         ),
       );
+
+      if (_attempts >= 3) {
+        _lockButton();
+      }
     }
+  }
+
+  void _lockButton() {
+    setState(() {
+      _isLocked = true;
+    });
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text("Terlalu banyak percobaan! Coba lagi nanti."),
+      ),
+    );
+
+    Future.delayed(const Duration(seconds: 10), () {
+      setState(() {
+        _isLocked = false;
+        _attempts = 0;
+      });
+    });
   }
 
   @override
@@ -51,7 +88,10 @@ class _LoginViewState extends State<LoginView> {
               decoration: const InputDecoration(labelText: "Password"),
             ),
             const SizedBox(height: 20),
-            ElevatedButton(onPressed: _handleLogin, child: const Text("Masuk")),
+            ElevatedButton(
+              onPressed: _isLocked ? null : _handleLogin,
+              child: Text(_isLocked ? "Tunggu 10 detik" : "Masuk"),
+            ),
           ],
         ),
       ),
