73a769d519d1b1d5c3e1961a909db439a106f0bb
raihanaiooo <raihanaaaz2@gmail.com>
2026-03-04 14:14:42 +0700

    UPDATE: tambahin folder logs

--- lib/helpers/log_helper.dart
+++ lib/helpers/log_helper.dart
@@ -1,6 +1,8 @@
 import 'dart:developer' as dev;
 import 'package:intl/intl.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 class LogHelper {
   static Future<void> writeLog(
@@ -9,7 +11,9 @@ class LogHelper {
     int level = 2,
   }) async {
     final int configLevel = int.tryParse(dotenv.env['LOG_LEVEL'] ?? '2') ?? 2;
+
     final String muteList = dotenv.env['LOG_MUTE'] ?? '';
+
     final mutedSources = muteList
         .split(',')
         .map((e) => e.trim())
@@ -17,21 +21,52 @@ class LogHelper {
         .toList();
 
     if (mutedSources.contains(source)) return;
-
     if (level > configLevel) return;
-    if (muteList.split(',').contains(source)) return;
 
     try {
       String timestamp = DateFormat('HH:mm:ss').format(DateTime.now());
+
       String label = _getLabel(level);
       String color = _getColor(level);
 
+      final logLine = "[$timestamp][$label][$source] -> $message";
+
       dev.log(message, name: source, time: DateTime.now(), level: level * 100);
 
-      print('$color[$timestamp][$label][$source] -> $message\x1B[0m');
+      print('$color$logLine\x1B[0m');
+
+      await _writeToFile(logLine);
     } catch (e) {
       dev.log("Logging failed: $e", name: "SYSTEM", level: 1000);
     }
+  }
+
+  static Future<void> _writeToFile(String content) async {
+    final now = DateTime.now();
+
+    final fileName =
+        "${now.day.toString().padLeft(2, '0')}-"
+        "${now.month.toString().padLeft(2, '0')}-"
+        "${now.year}.log";
+
+    final directory = await getExternalStorageDirectory();
+
+    if (directory == null) {
+      print("External storage tidak tersedia");
+      return;
+    }
+
+    final logDir = Directory('${directory.path}/logs');
+
+    if (!await logDir.exists()) {
+      await logDir.create(recursive: true);
+    }
+
+    final file = File('${logDir.path}/$fileName');
+
+    await file.writeAsString(content + "\n", mode: FileMode.append);
+
+    print("LOG FILE PATH: ${file.path}");
   }
 
   static String _getLabel(int level) {
